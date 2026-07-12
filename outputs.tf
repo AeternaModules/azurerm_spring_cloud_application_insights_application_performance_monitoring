8593ad9f4274ca69729e5f1cd19855eebb4a2384
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "spring_cloud_application_insights_application_performance_monitorings" {
-  description = "All spring_cloud_application_insights_application_performance_monitoring resources"
-  value       = azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings
-}
 output "spring_cloud_application_insights_application_performance_monitorings_connection_string" {
-  description = "List of connection_string values across all spring_cloud_application_insights_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : v.connection_string]
+  description = "Map of connection_string values across all spring_cloud_application_insights_application_performance_monitorings, keyed the same as var.spring_cloud_application_insights_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : k => v.connection_string }
 }
 output "spring_cloud_application_insights_application_performance_monitorings_globally_enabled" {
-  description = "List of globally_enabled values across all spring_cloud_application_insights_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : v.globally_enabled]
+  description = "Map of globally_enabled values across all spring_cloud_application_insights_application_performance_monitorings, keyed the same as var.spring_cloud_application_insights_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : k => v.globally_enabled }
 }
 output "spring_cloud_application_insights_application_performance_monitorings_name" {
-  description = "List of name values across all spring_cloud_application_insights_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : v.name]
+  description = "Map of name values across all spring_cloud_application_insights_application_performance_monitorings, keyed the same as var.spring_cloud_application_insights_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : k => v.name }
 }
 output "spring_cloud_application_insights_application_performance_monitorings_role_instance" {
-  description = "List of role_instance values across all spring_cloud_application_insights_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : v.role_instance]
+  description = "Map of role_instance values across all spring_cloud_application_insights_application_performance_monitorings, keyed the same as var.spring_cloud_application_insights_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : k => v.role_instance }
 }
 output "spring_cloud_application_insights_application_performance_monitorings_role_name" {
-  description = "List of role_name values across all spring_cloud_application_insights_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : v.role_name]
+  description = "Map of role_name values across all spring_cloud_application_insights_application_performance_monitorings, keyed the same as var.spring_cloud_application_insights_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : k => v.role_name }
 }
 output "spring_cloud_application_insights_application_performance_monitorings_sampling_percentage" {
-  description = "List of sampling_percentage values across all spring_cloud_application_insights_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : v.sampling_percentage]
+  description = "Map of sampling_percentage values across all spring_cloud_application_insights_application_performance_monitorings, keyed the same as var.spring_cloud_application_insights_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : k => v.sampling_percentage }
 }
 output "spring_cloud_application_insights_application_performance_monitorings_sampling_requests_per_second" {
-  description = "List of sampling_requests_per_second values across all spring_cloud_application_insights_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : v.sampling_requests_per_second]
+  description = "Map of sampling_requests_per_second values across all spring_cloud_application_insights_application_performance_monitorings, keyed the same as var.spring_cloud_application_insights_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : k => v.sampling_requests_per_second }
 }
 output "spring_cloud_application_insights_application_performance_monitorings_spring_cloud_service_id" {
-  description = "List of spring_cloud_service_id values across all spring_cloud_application_insights_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : v.spring_cloud_service_id]
+  description = "Map of spring_cloud_service_id values across all spring_cloud_application_insights_application_performance_monitorings, keyed the same as var.spring_cloud_application_insights_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_application_insights_application_performance_monitoring.spring_cloud_application_insights_application_performance_monitorings : k => v.spring_cloud_service_id }
 }
 
